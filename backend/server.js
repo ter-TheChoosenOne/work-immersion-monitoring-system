444eb0dc15886367529fd74c8279a9d6c90811b9
Dexter <dexterrilles06@gmail.com>
2026-04-14 19:40:42 +0800

    test1

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 connectDB();
 
 app.use(cors({
-   origin: "*"
+   origin: [
+    'http://localhost:5173'
+   ],
+   credentials: true
+
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
